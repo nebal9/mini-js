5310eb60d85cdda7dca3ca81d2790cb36d8b9574
nebal9 <nebal.9801@gmail.com>
2024-08-04 13:12:47 +0800

    limit speed + control blocks

--- GnB/GnB.js
+++ GnB/GnB.js
@@ -7,14 +7,35 @@ const KeyMapper = {
     '37': { actor: "banana", action: "left" }, //left
     '40': { actor: "banana", action: "down" }, //down
     '39': { actor: "banana", action: "right" }, //right
+
+    '189': { actor: 'system', action: 'decreaseSpeed' }, //-
+    '187': { actor: 'system', action: 'increaseSpeed' }, //+
+    '219': { actor: 'system', action: 'decreaseTrees' }, //[
+    '221': { actor: 'system', action: 'increaseTrees' }, //]
 };
 
 const MapWidth = 8;
 const MapHeight = 6;
 
-const bloackedTiles = [
-    "c_3_3", "c_4_6", "c_2_5",
+let DelayTime = 1000;
+let DelayTimeInterval = 200;
+
+let NumberOfTrees = 3;
+
+const suggestedBlockedTiles = [
+    "c_1_4",
+    "c_3_7",
+    "c_5_5",
+    "c_4_2",
+    "c_3_3",
+    "c_2_7",
+    "c_4_5",
+    "c_6_2",
+    "c_1_3",
+    "c_6_7",
 ];
+
+let bloackedTiles = [...suggestedBlockedTiles.slice(0, NumberOfTrees)];
 let gorillaPosition = "c_1_1";
 let bananaPosition = "c_6_8";
 
@@ -94,33 +115,78 @@ const getNextPosition = (x, y, action) => {
     };
 };
 
-const moveItem = (keyCode) => {
-    var keyParams = KeyMapper[keyCode];
+const lastMovements = {
+    gorilla: new Date(),
+    banana: new Date(),
+};
+
+const actorCanPlay = (actor) => {
+    if (new Date() - lastMovements[actor] >= DelayTime) {
+        lastMovements[actor] = new Date();
+        return true;
+    } else {
+        return false;
+    };
+};
+
+const moveItem = (actor, action) => {
+    if (!actorCanPlay(actor)) return;
     setStartDateMaybe();
 
+    var isGorilla = actor == "gorilla";
+    var currentPosition = isGorilla ? gorillaPosition : bananaPosition;
+
+    var [c, y, x] = currentPosition.split("_");
+    var [nx, ny] = getNextPosition(Number(x), Number(y), action);
+
+    if (!isCellBlocked(nx, ny) && !isCellOutOfMap(nx, ny)) {
+        let nextPosition = `c_${ny}_${nx}`;
+
+        if (isGorilla) {
+            gorillaPosition = nextPosition;
+            if (nextPosition == bananaPosition) {
+                endGame();
+            };
+        } else {
+            bananaPosition = nextPosition;
+            if (nextPosition == gorillaPosition) {
+                endGame();
+            };
+        };
+        initiateMap();
+    };
+};
+
+const initiateSystemAction = (action) => {
+    if (action == "decreaseSpeed") {
+        DelayTime += DelayTimeInterval;
+    } else if (action == "increaseSpeed") {
+        if (DelayTime > 0) {
+            DelayTime -= DelayTimeInterval;
+        };
+    } else if (action == "decreaseTrees") {
+        if (NumberOfTrees >= 1) {
+            NumberOfTrees -= 1;
+            bloackedTiles = [...suggestedBlockedTiles.slice(0, NumberOfTrees)];
+            initiateMap();
+        };
+    } else if (action == "increaseTrees") {
+        if (NumberOfTrees < suggestedBlockedTiles.length) {
+            NumberOfTrees += 1;
+            bloackedTiles = [...suggestedBlockedTiles.slice(0, NumberOfTrees)];
+            initiateMap();
+        };
+    };
+};
+
+const checkKeyAction = (keyCode) => {
+    var keyParams = KeyMapper[keyCode];
     if (keyParams) {
         const { actor, action } = keyParams;
-        var isGorilla = actor == "gorilla";
-        var currentPosition = isGorilla ? gorillaPosition : bananaPosition;
-
-        var [c, y, x] = currentPosition.split("_");
-        var [nx, ny] = getNextPosition(Number(x), Number(y), action);
-
-        if (!isCellBlocked(nx, ny) && !isCellOutOfMap(nx, ny)) {
-            let nextPosition = `c_${ny}_${nx}`;
-
-            if (isGorilla) {
-                gorillaPosition = nextPosition;
-                if (nextPosition == bananaPosition) {
-                    endGame();
-                };
-            } else {
-                bananaPosition = nextPosition;
-                if (nextPosition == gorillaPosition) {
-                    endGame();
-                };
-            };
-            initiateMap();
+        if (actor == "system") {
+            initiateSystemAction(action);
+        } else {
+            moveItem(actor, action);
         };
     };
 };
@@ -146,9 +212,7 @@ window.onload = () => {
 
     document.addEventListener("keydown", event => {
         if (!event.isComposing) {
-            moveItem(event.keyCode);
+            checkKeyAction(event.keyCode);
         };
     });
 };
-
-// to do: strict movement by time
